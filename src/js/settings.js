c7a750da6f1f4c9f8110aa3f139a6d2b3665ac0a
Marcin Grajdek <grajdek.m@vp.pl>
2022-05-24 14:05:55 +0200

    Add init project- coffee

---
+++ src/js/settings.js
@@ -0,0 +1,14 @@
+export const settings = {
+
+  db: {
+    //url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
+    /*products: 'products',
+    orders: 'orders',
+    booking: 'bookings',*/
+    products: 'products',
+    contact: 'contact',
+    home: 'home',
+    
+  },
+};
